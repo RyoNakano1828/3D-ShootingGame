0c357142017c2e634e821ac73b3406cd39f3398a
ryo_nakano <naca.11868@gmail.com>
2021-01-04 17:06:58 +0900

    3D Shooting Game

---
+++ Assets/Scripts/Enemy/MyEnemyAttack.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyEnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+    Animator anim;
+    GameObject player;
+    MyPlayerHealth myPlayerHealth;
+    bool playerInRange;
+    float timer;
+    MyEnemyHealth myEnemyHealth;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        myPlayerHealth = player.GetComponent<MyPlayerHealth>();
+        anim = GetComponent<Animator>();
+        myEnemyHealth = GetComponent<MyEnemyHealth>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        
+        if(timer >= timeBetweenAttacks && playerInRange && myEnemyHealth.currentHealth > 0)
+        {
+            Attack();
+        }
+            
+        if(myPlayerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("PlayerDead");
+        }
+    }
+
+    void Attack()
+    {
+        timer = 0;
+
+        if(myPlayerHealth.currentHealth > 0)
+        {
+            myPlayerHealth.TakeDamage(attackDamage);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/MyEnemyMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyEnemyMovement : MonoBehaviour
+{
+    Transform player;
+    UnityEngine.AI.NavMeshAgent nav;
+    MyPlayerHealth myPlayerHealth;
+    MyEnemyHealth myEnemyHealth;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        myPlayerHealth = player.GetComponent<MyPlayerHealth>();
+        myEnemyHealth = GetComponent<MyEnemyHealth>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(myEnemyHealth.currentHealth > 0 && myPlayerHealth.currentHealth > 0)
+        {
+            nav.SetDestination(player.position);
+        }
+        else
+        {
+            nav.enabled = false;
+        }
+    }
+}
